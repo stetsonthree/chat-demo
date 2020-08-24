7de8e5fe64d5f03d63f51b1c3ef751104297aeef
Lee Stetson <stetsonthree@icloud.com>
2020-08-24 00:23:00 -0400

    Hooks up functional messaging
    - Transmits messages
    - Unsubscribes subscriptions when changing rooms

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -25,11 +25,10 @@ class MessagesController < ApplicationController
   # POST /messages.json
   def create
     @message = Message.new(message_params)
-
     @message.user = current_user
-
     @message.save
-    redirect_to request.referrer
+
+    SendMessageJob.perform_later(@message)
   end
 
   # PATCH/PUT /messages/1
--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -1,25 +1,40 @@
 import consumer from "./consumer"
 
 document.addEventListener('turbolinks:load', () => {
+  const room_element = document.getElementById('room-id');
+  const room_id = Number(room_element.getAttribute('data-room-id'));
 
-  const element = document.getElementById('room-id');
-  const room_id = element.getAttribute('data-room-id');
+  console.log(consumer.subscriptions)
 
-  console.log(room_id);
-
+  consumer.subscriptions.subscriptions.forEach((subscription) => {
+    consumer.subscriptions.remove(subscription)
+  })
+  
   consumer.subscriptions.create({ channel: "RoomChannel", room_id: room_id }, {
     connected() {
-      // Called when the subscription is ready for use on the server
       console.log("Connected to room channel " + room_id)
+      // Called when the subscription is ready for use on the server
     },
-  
+    
     disconnected() {
       // Called when the subscription has been terminated by the server
     },
-  
+    
     received(data) {
       // Called when there's incoming data on the websocket for this channel
-      console.log(data)
+      const user_element = document.getElementById('user-id');
+      const user_id = Number(user_element.getAttribute('data-user-id'));
+
+      let html;
+
+      if (user_id === data.message.user_id) {
+        html = data.mine
+      } else {
+        html = data.theirs
+      }
+
+      const messageContainer = document.getElementById('messages')
+      messageContainer.innerHTML = messageContainer.innerHTML + html
     }
   });
 })
